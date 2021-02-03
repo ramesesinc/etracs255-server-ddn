4af1e94d50796f6b7104b9db8183318bfd869282
worgflores <worgflores@gmail.com>
2021-02-03 13:07:28 +0800

    updated

---
+++ _res/patches/v255_20201112_001.patch/mysql/01-patch-etracs-lgu-specific.sql
@@ -0,0 +1,9 @@
+
+alter table business add activeqtr int null ;
+alter table business add apptype	varchar(50) null; 
+alter table business add oldbin	varchar(50)	null ; 
+alter table business add permit_objid  varchar(50)	null; 
+alter table business add permittype varchar(50) null ;
+alter table business add expirydate	date	null;
+alter table business add officetype	varchar(25)	null;
+alter table business add nextrenewaldate	date	null;
